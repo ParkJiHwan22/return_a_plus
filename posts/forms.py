bbb5244cfe02bd01bfab007a2e9042f4882910ba
ParkJjHwan22 <bat522@naver.com>
2023-05-04 15:49:35 +0900

    Add detail_icon

--- posts/forms.py
+++ posts/forms.py
@@ -29,6 +29,15 @@ class PostForm(forms.ModelForm):
         )
     )
     
+    price = forms.CharField(
+        label='가격(성인 기준)',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+    
     description = forms.CharField(
         label= '관광지 설명',
         widget= forms.Textarea(
@@ -45,16 +54,55 @@ class PostForm(forms.ModelForm):
         required=False,
     )
 
+
+    checkbox1 = forms.BooleanField(required=False, label='문화 및 역사 유적지')
+    checkbox2 = forms.BooleanField(required=False, label='자연 경관')
+    checkbox3 = forms.BooleanField(required=False, label='테마파크')
+    checkbox4 = forms.BooleanField(required=False, label='어드벤처 및 스포츠')
+    checkbox5 = forms.BooleanField(required=False, label ='축제 및 이벤트')
+    checkbox6 = forms.BooleanField(required=False, label='먹거리')
+    checkbox7 = forms.BooleanField(required=False, label='쇼핑')
+    checkbox8 = forms.BooleanField(required=False, label='가족 나들이')
+    checkbox9 = forms.BooleanField(required=False, label='데이트 코스')
+    checkbox10 = forms.BooleanField(required=False, label = '셀카 명소')
+    
+    
+    
     class Meta:
         model = Post
         fields = (
             "post_image",
             "name",
             "city",
-            "address", 
-            "description",  
+            "address",
+            "price", 
+            "description",
+            "checkbox1",
+            "checkbox2",
+            "checkbox3",
+            "checkbox4",
+            "checkbox5",
+            "checkbox6",
+            "checkbox7",
+            "checkbox8",
+            "checkbox9",
+            "checkbox10",
         )
+        
+        widgets = {
+            'checkbox1': forms.CheckboxInput(),
+            'checkbox2': forms.CheckboxInput(),
+            'checkbox3': forms.CheckboxInput(),
+            'checkbox4': forms.CheckboxInput(),
+            'checkbox5': forms.CheckboxInput(),
+            'checkbox6': forms.CheckboxInput(),
+            'checkbox7': forms.CheckboxInput(),
+            'checkbox8': forms.CheckboxInput(),
+            'checkbox9': forms.CheckboxInput(),
+            'checkbox10': forms.CheckboxInput(),
+        }
 
+    
 
 REVIEW_POINT_CHOICES = (
         ("★☆☆☆☆ (1/5)", "★☆☆☆☆ (1/5)"),
--- posts/views.py
+++ posts/views.py
@@ -34,6 +34,7 @@ colors = [
 color_dict = {}
 color_index = 0
 
+
 # Create your views here.
 def index(request):
     global color_index
@@ -59,6 +60,7 @@ def index(request):
     }
     return render(request, 'posts/index.html', context)
 
+
 @login_required
 def create(request):
     if request.method == "POST":
@@ -71,7 +73,7 @@ def create(request):
     else:
         form = PostForm()
     context = {
-        'form':form
+        'form':form,
     }
     return render(request, 'posts/create.html', context)
 
@@ -95,12 +97,14 @@ def detail(request, posts_pk):
     }
     return render(request, 'posts/detail.html', context,)
 
+
 @login_required
 def delete(request, posts_pk):
     post = Post.objects.get(pk=posts_pk)
     if post.user == request.user:
         post.delete()
     return redirect('posts:index')
+
 
 @login_required
 def update(request, posts_pk):
@@ -119,6 +123,7 @@ def update(request, posts_pk):
         return render(request, 'posts/update.html', context)
     else:
         return redirect('posts:detail', post.pk)
+
 
 @login_required
 def review_create(request, posts_pk):
@@ -152,6 +157,7 @@ def like(request, post_pk):
     else:
         post.like_users.add(request.user)
     return redirect('posts:index')
+
 
 def about_us(request):
     return render(request, 'posts/about_us.html')
